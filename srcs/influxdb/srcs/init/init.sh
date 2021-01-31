658ea55fe438f25f3a99e07db7b62261ac3e603e
00101010 <00101010@00101010.fr>
2021-01-31 01:02:43 +0100

    roomba go brrrr (cleaned the project from developement files, scripts and dockerfile rules)

--- srcs/influxdb/srcs/init/init.sh
+++ srcs/influxdb/srcs/init/init.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-influxd & influx < /tmp/db.conf
+/etc/init.d/influxdb setup
+service influxdb start
+FLAG="$(echo 'SHOW DATABASES;' | influx | grep grafana)"
+if [ -z "$FLAG" ] ; then
+	influx < /tmp/db.conf
+fi
 sleep infinity
--- srcs/mysql/srcs/init/init.sh
+++ srcs/mysql/srcs/init/init.sh
@@ -1,11 +1,10 @@
 #!/bin/ash
+/etc/init.d/mariadb setup
+/usr/bin/mysql_install_db --datadir="/var/lib/mysql";
+service mariadb start
 FLAG="$(echo 'SHOW DATABASES;' | mysql | grep wordpress)"
 if [ -z "$FLAG" ] ; then
-	/usr/bin/mysql_install_db --datadir="/var/lib/mysql";
-	service mariadb start
 	cat  /tmp/mysql.conf | mysql -u root 
 	mysql wordpress -u root < /tmp/wordpress_dump.sql
-	/etc/init.d/mariadb setup
 fi
-service mariadb start
 sleep infinity
